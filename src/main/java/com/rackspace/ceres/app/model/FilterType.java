64a8149dff734a776dbd7a3b3d1f153ac854bd3f
mats3569 <mats.boman@rackspace.com>
2022-05-29 01:32:08 -0500

    Test branch for rollups jobs service

--- src/main/java/com/rackspace/ceres/app/model/FilterType.java
+++
@@ -1,5 +0,0 @@
-package com.rackspace.ceres.app.model;
-
-public enum FilterType {
-    literal_or
-}
--- src/main/java/com/rackspace/ceres/app/model/TagFilter.java
+++
@@ -1,5 +0,0 @@
-package com.rackspace.ceres.app.model;
-
-public enum TagFilter {
-  EXCLUDE, TRUNCATE
-}
