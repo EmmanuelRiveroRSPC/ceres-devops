9806275f0503f9adb422f86bb41fc18dc51fda27
Emmanuel Rivero <emmanuel.rivero@rackspace.com>
2022-07-26 15:41:18 -0500

    testing pyyaml

---
+++ test-scripts/runTest.py
@@ -0,0 +1,20 @@
+import yaml
+import argparse
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('-l', '--list', help='Test list file path', default="." )
+parser.add_argument('-t', '--test_set', help='Test set to be run', Required=True)
+
+args=parser.parse_args()
+
+listPath = args.list
+testSet = args.test_set
+
+
+with open(listPath) as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    testSet = yaml.load(file, Loader=yaml.FullLoader)
+
+    print(testSet)
