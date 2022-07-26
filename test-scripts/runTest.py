f5e1471be78b3079777d4a8ddd409502a5737c25
Emmanuel Rivero <emmanuel.rivero@rackspace.com>
2022-07-26 16:34:44 -0500

    testing types

--- test-scripts/runTest.py
+++ test-scripts/runTest.py
@@ -1,5 +1,16 @@
 import yaml
 import argparse
+import subprocess
+
+def runPython(testName, arguments):
+    cmd = ["python", testName, arguments]
+    print (cmd)
+
+def runJava(testName, arguments):
+    cmd = ["java", "-jar", testName, arguments]
+    print (cmd)
+
+suportedTypes = ['python', 'java']
 
 parser=argparse.ArgumentParser()
 
@@ -11,10 +22,23 @@ args=parser.parse_args()
 listPath = args.list
 testSet = args.test_set
 
-testSet = {}
+alltestSet = {}
 with open(listPath) as file:
     # The FullLoader parameter handles the conversion from YAML
     # scalar values to Python the dictionary format
-    testSet = yaml.load(file, Loader=yaml.FullLoader)
+    alltestSet = yaml.load(file, Loader=yaml.FullLoader)
 
-print (testSet)
+for test in alltestSet[testSet]:
+    if test["type"] == "python":
+        runPython(testName=test["name"], arguments=test["argLine"])
+
+    if test["type"] == "java":
+        print("tyolo")
+    
+    if test["type"] not in suportedTypes:
+        print ("Type: {} not suported for test: {}".format(test["type"], test["name"]))
+        print ("Suported types:")
+        for appType in suportedTypes:
+            print (f"  {appType}")
+    
+    
