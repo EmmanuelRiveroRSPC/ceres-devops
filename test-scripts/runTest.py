a43865d6d4667c98717a97a522098511be75d2f2
Emmanuel Rivero <emmanuel.rivero@rackspace.com>
2022-07-27 15:02:33 -0500

    encapsulating

--- test-scripts/runTest.py
+++ test-scripts/runTest.py
@@ -16,16 +16,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
     RESETALL = '\033[0m'
 
-def runPython(testName, arguments, testDirectory):
-    localPath = f"{testDirectory}/{testName}.py"
-
-    if not os.path.isfile(localPath):
-        print ("not in local")
-
-    cmd = ["python", localPath]
-
-    argumentsList = arguments.split()
-    cmd.extend(argumentsList)
+def runProcess(cmd):
     Process = subprocess.run(cmd, stdout=subprocess.PIPE)
     Text = Process.stdout.decode('utf-8')
     print (Text)
@@ -35,61 +26,83 @@ def runPython(testName, arguments, testDirectory):
     else:
         return False
 
-def runJava(testName, arguments):
-    cmd = ["java", "-jar", testName]
+def runTest(testName, arguments, testDirectory, testType):
+    localPath = f"{testDirectory}/{testName}.py"
 
-    argumentsList = arguments.split()
-    cmd.extend(argumentsList)
+    if not os.path.isfile(localPath):
+        print ("not in local")
 
-    print (cmd)
+    cmd = []
+    if testType == "python":
+        cmd = ["python"]
+    if testType == "java":
+        cmd = ["java", "-jar"]
 
-suportedTypes = ['python', 'java']
-
-parser=argparse.ArgumentParser()
-
-parser.add_argument('-l', '--list', help='Test list file path', default="./test-plan.yml" )
-parser.add_argument('-t', '--test_set', help='Test set to be run', required=True)
-parser.add_argument('-d', '--test_directory', help='local test directory', default=".")
-
-args=parser.parse_args()
-
-listPath = args.list
-testSet = args.test_set
-testDirectory=args.test_directory
-
-alltestSet = {}
-with open(listPath) as file:
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
-    alltestSet = yaml.load(file, Loader=yaml.FullLoader)
-
-SummaryList = []
-
-for test in alltestSet[testSet]:
-    if test["type"] == "python":
-        print ("Starting test: {}".format(test["name"]) )
-        result = ""
-        if runPython(testName=test["name"], arguments=test["argsLine"], testDirectory=testDirectory):
-            result = "Pass"
-        else:
-            result = "Failed"
-        SummaryList.append({"name":test["name"], "result":result})
-
-    if test["type"] == "java":
-        runJava(testName=test["name"], arguments=test["argsLine"])
-    
-    if test["type"] not in suportedTypes:
+    if test["type"] not in suportedTypes.suppoerted:
         print ("Type: {} not suported for test: {}".format(test["type"], test["name"]))
         print ("Suported types:")
-        for appType in suportedTypes:
+        for appType in suportedTypes.suppoerted:
             print (f"  {appType}")
+        exit(1)
+
+    cmd.append(localPath)
     
-print ("All test complete")
-print (bcolors.HEADER + "Summary:" + bcolors.RESETALL)
-for test in SummaryList:
-    printable = ""
-    if test["result"] == "PASS":
-        printable = bcolors.OKGREEN + test["result"]
+    argumentsList = arguments.split()
+    cmd.extend(argumentsList)
+    if runProcess(cmd):
+        result = "Pass"
     else:
-        printable = bcolors.FAIL + test["result"]
-    print (" {}:{}{}".format(test["name"], printable, bcolors.RESETALL))
+        result = "Failed"
+    
+    return {"name":test["name"], "result":result}
+
+class Artifactory:
+    url = ""
+    repository = ""
+    skip = True
+
+class suportedTypes:
+    suppoerted =  ['python', 'java']
+
+
+def main():
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument('-l', '--list', help='Test list file path', default="./test-plan.yml" )
+    parser.add_argument('-t', '--test_set', help='Test set to be run', required=True)
+    parser.add_argument('-d', '--test_directory', help='local test directory', default=".")
+
+    args=parser.parse_args()
+
+    listPath = args.list
+    testSet = args.test_set
+    testDirectory=args.test_directory
+
+    alltestSet = {}
+    with open(listPath) as file:
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
+        alltestSet = yaml.load(file, Loader=yaml.FullLoader)
+
+    if "Artifactory" in alltestSet:
+        Artifactory.url = alltestSet["Artifactory"]["url"]
+        Artifactory.repository = alltestSet["Artifactory"]["repository"]
+        Artifactory.skip = False
+
+    SummaryList = []
+
+    for test in alltestSet[testSet]:
+        SummaryList.append(runTest(testName=test["name"], arguments=test["argsLine"], testDirectory=testDirectory, testType=test["type"]))
+        
+    print ("All test complete")
+    print (bcolors.HEADER + bcolors.BOLD + "Summary:" + bcolors.RESETALL)
+    for test in SummaryList:
+        printable = ""
+        if test["result"] == "Pass":
+            printable = bcolors.OKGREEN + test["result"]
+        else:
+            printable = bcolors.FAIL + test["result"]
+        print (" {}: {}{}".format(test["name"], printable, bcolors.RESETALL))
+
+if __name__ == "__main__":
+    main()
